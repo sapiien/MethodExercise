64ce3977cd5eb3a5ff510174f670d566a1c2c475
Kelsey Sapien <samsapien927@gmail.com>
2022-07-05 19:57:06 -0500

    commit

---
+++ MethodExercise/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MethodExercise
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
